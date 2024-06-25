3c7bff7a4a108197dd9c5cf481028a9583b413e2
Vaishnavi35 <89574212+Vaishnavi35@users.noreply.github.com>
2024-06-25 11:09:05 +0530

    load balanced

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("demo app");
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@Bean
+   @LoadBalanced
+   public RestTemplate restTemplate(){
+      return new RestTemplate();
+   }
 }
